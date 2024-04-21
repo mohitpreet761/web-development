be1240789ccf1cb9032c2a889f1612dc77c2ff58
mohitpreet761 <166578443+mohitpreet761@users.noreply.github.com>
2024-04-21 13:53:10 +1200

    Create sub.py

---
+++ create research file/sub.py
@@ -0,0 +1,9 @@
+# Get input from the user
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# Subtract the numbers
+difference = num1 - num2
+
+# Display the result
+print("The difference of", num1, "and", num2, "is", difference)
